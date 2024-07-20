7a167109bc18355fb38cb216c68674e3ba0bb868
rva174 <rva174@yandex.ru>
2024-07-20 10:49:51 +0500

    Initial commit

---
+++ homework2.py
@@ -0,0 +1,5 @@
+total_tasks = 12
+total_hours = 1.5
+average_time = total_hours / total_tasks
+
+print('Курс: Python, ' 'Всего задач:', total_tasks,', Затрачено часов:', total_hours,', Среднее время выполнения:', average_time,'часа')
---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Viktor'
+print(name)
+age = 66
+print('Age:', age)
+age = (age + 34)
+print('New age:', age)
+is_student = True
+print(is_student)
