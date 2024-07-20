32684fba905d34ecca8366bc7a415a91f6456373
rva174 <rva174@yandex.ru>
2024-07-20 20:42:33 +0500

    Create homework4.py

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_spring = input('Где находится озеро Тургояк? ')
+# Озеро Тургояк находится в Челябинской области
+print('Количество символов строки:',len(my_spring))
+print(my_spring.upper())
+print(my_spring.lower())
+print(my_spring.replace (' ',''))
+print('Первый символ строки:',my_spring[0])
+print('Последний символ строки:',my_spring[-1])
