178c4f487f7c2d69e15a205cbeaf0735b75ebaad
rva174 <rva174@yandex.ru>
2024-07-25 22:08:34 +0500

    Не прнименял:break, contin (??)

--- homework6.py
+++ homework6.py
@@ -1,28 +1,25 @@
-my_dict = {'Владимир': 1870, 'Fedor': 2000, 'Анна': 2002}
-print(type(my_dict))
-print('Dict:', my_dict)
-print(my_dict['Владимир']) # Вывод значения по существующему ключу
-print(my_dict.get('Байден')) # Ключ отсутствует
-my_dict.update({'Petr': 2001,
-               'Maria': 2016})
-print('Modificed dictionary:', my_dict)
-a = my_dict.pop('Fedor')
-print('Deleted value:', a)
-print('Modificed dictionary:', my_dict)
-
-my_set = {1, 3, 6, 'dog', 'cat'}
-print('My_set:', my_set)
-my_set.add(5)
-my_set.add('goal')
-print('Modificired my_set:', my_set)
-my_set.discard(3)
-print('Modificired my_set:', my_set)
-
-
-
-
-
-
-
-
-
+immutable_var = True, 'urban', 5, 10.5
+print(type(immutable_var))
+print('immutable_var:',immutable_var)
+#immutable_var[0] = True
+#immutable_var[1] = 25
+#immutable_var[-1] = 10
+print('immutable_var:',immutable_var)
+#Элементы кортежа неизменяемые
+mutable_list = ['table', 'chair', 'sofa', 3]
+print('mutable_list:', mutable_list)
+mutable_list.append(True)
+print('mutable_list:', mutable_list)
+print('Добавлен элемент в конец списка')
+mutable_list.extend('goal')
+print('mutable_list:', mutable_list)
+print('Символы строк goal добавлены по одному')
+mutable_list.extend(['goal', 4])
+mutable_list[0] = 'desk'
+print('mutable_list:', mutable_list)
+print('Строка goal не разобрана на символы,она здесь элемент списка,')
+print('      заменен перрвый элемент списка')
+mutable_list.remove('chair')
+print('mutable_list:', mutable_list)
+print('  Элемент chair  исключен из списка')
+print('  Элемент chair  исключен из списка')
