5abf0a024a24ba66617357627efe695cc345899e
rva174 <rva174@yandex.ru>
2024-07-26 17:34:25 +0500

    исправлено

--- module_2_3.py
+++ module_2_3.py
@@ -1,17 +1,16 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-print('my_list: ',my_list)
+print('my_list: ', my_list)
 numbers = len(my_list)
-print('Количество элементов в списке: ',numbers)
+print('Количество элементов в списке: ', numbers)
 
-number = 0               # индекс первого элемента списка, от 0 до 11
-print(my_list[number])   #
+number = 0  # Индекс элемента списка, от 0 до (numbers - 1)
 
-while number <= ((numbers) - 1):
-    if my_list[number] < 0:
-        print('Отрицательное число: ',my_list[number])
+while(number <= (numbers) - 1):
+    print(my_list[number])
     number = number + 1
-else:
-    print ('Перебор элементов списка завершен')
+    if my_list[number] < 0:
+        break
+print('Перебор элементов списка завершен')
 
 
 
