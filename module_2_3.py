8f145168dba0e409248025d593e5d22c84d89899
rva174 <rva174@yandex.ru>
2024-07-26 21:00:52 +0500

    Изм.2

--- module_2_3.py
+++ module_2_3.py
@@ -1,17 +1,13 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-print('my_list: ', my_list)
-numbers = len(my_list)
-print('Количество элементов в списке: ', numbers)
+print('my_list: ',my_list)
 
-number = 0  # Индекс элемента списка, от 0 до (numbers - 1)
-
-while(number <= (numbers) - 1):
-    print(my_list[number])
-    number = number + 1
-    if my_list[number] < 0:
+index = 0
+while index < len(my_list):
+    if my_list[index] >= 0:
+        print(my_list[index])
+        index = index + 1
+    if my_list[index] < 0:
         break
-print('Перебор элементов списка завершен')
-
 
 
 
