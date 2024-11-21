25b325daeabdb22f35765fbfd679d2b75a09c4f7
rva174 <rva174@yandex.ru>
2024-11-21 17:57:40 +0500

    ф

--- module_2_3.py
+++ module_2_3.py
@@ -1,12 +1,12 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-print('my_list: ',my_list)
 
 index = 0
 while index < len(my_list):
-    if my_list[index] > 0:
-        print(my_list[index])
+    if my_list[index] == 0:
+        index += 1
+        continue
     if my_list[index] < 0:
         break
+    print(my_list[index])
     index = index + 1
-    if my_list[index] == 0:
-        continue
+
